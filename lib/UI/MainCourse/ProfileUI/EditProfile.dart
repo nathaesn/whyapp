26eed0ad538c01d768f5c745e4998a38c2ea2f56
andikanatha <setya.natha@gmail.com>
2023-02-03 08:19:08 +0700

    style [AN] add UI Edit Profile

--- lib/UI/MainCourse/ProfileUI/EditProfile.dart
+++ lib/UI/MainCourse/ProfileUI/EditProfile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whyapp/Theme.dart';
 
 class EditProfileUI extends StatefulWidget {
   EditProfileUI({Key? key}) : super(key: key);
@@ -17,6 +18,65 @@ class _EditProfileUIState extends State<EditProfileUI> {
           style: TextStyle(fontSize: 15),
         ),
         elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            
+            decoration: BoxDecoration(
+              color: secondarycolor,),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 45),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: SizedBox(
+                      height: 130,
+                      width: 130,
+                      child: Image.network(
+                        "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "Nama",
+                labelStyle: TextStyle(color: surfacecolor),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: surfacecolor),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: surfacecolor),
+                ),
+              ),
+              style: TextStyle(color: surfacecolor),
+            ),
+          ),
+          Spacer(),
+          SizedBox(
+            height: 50,
+            width: 203,
+            child: ElevatedButton(onPressed: (){}, 
+              style: ElevatedButton.styleFrom(
+                elevation: 0,
+                primary: secondarycolor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                )
+              ),
+            child: Text ("Simpan")),
+          ),
+          Spacer()
+        ],
       ),
     );
   }
